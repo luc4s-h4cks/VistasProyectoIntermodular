9342f973bb82aa4b39199611c8063161aafed3e2
lucas curiel <lcurgom2406@iesmarquesdecomares.org>
2026-01-15 21:24:41 +0100

    añadido el pop up de cada dia y mejorado y añadido el pop up de los detalles de una solicitud

--- JS/JS_VistaGestionCistas.js
+++ JS/JS_VistaGestionCistas.js
@@ -1,47 +1,78 @@
-const d = document
+const d = document;
 
 d.addEventListener("click", (e) => {
-    if (e.target.matches('#solicitudes')) {
-        d.getElementById("content-solicitudes").classList.remove("hidden")
-        d.getElementById("content-notificaciones").classList.add("hidden")
-    }
+  if (e.target.matches("#solicitudes")) {
+    d.getElementById("content-solicitudes").classList.remove("hidden");
+    d.getElementById("content-notificaciones").classList.add("hidden");
+  }
 
-    if (e.target.matches('#notificaciones')) {
-        d.getElementById("content-notificaciones").classList.remove("hidden")
-        d.getElementById("content-solicitudes").classList.add("hidden")
-    }
-})
+  if (e.target.matches("#notificaciones")) {
+    d.getElementById("content-notificaciones").classList.remove("hidden");
+    d.getElementById("content-solicitudes").classList.add("hidden");
+  }
+});
 
 function crearCalendario() {
-    console.log("hola")
-    let calendario = d.getElementById("calendario")
-    for (let i = 1; i < 31; i++) {
-        let dia = d.createElement("button")
-        dia.textContent = i
-        dia.id = "dia" + i
-        dia.classList.add("border")
-        dia.classList.add("rounded")
-        dia.classList.add("p-2")
-        dia.classList.add("hover:bg-gray-200")
-        dia.addEventListener("click", ()=>{
-            mostrarModalDia(i)
-        })
-        calendario.appendChild(dia)
-    }
-
+  console.log("hola");
+  let calendario = d.getElementById("calendario");
+  for (let i = 1; i < 31; i++) {
+    let dia = d.createElement("button");
+    dia.textContent = i;
+    dia.id = "dia" + i;
+    dia.classList.add("border");
+    dia.classList.add("rounded");
+    dia.classList.add("p-2");
+    dia.classList.add("hover:bg-gray-200");
+    dia.addEventListener("click", () => {
+      mostrarModalDia(i);
+    });
+    calendario.appendChild(dia);
+  }
 }
 
-function mostrarModalDia(dia){
-    d.getElementById("modalDiaText").textContent = "Se a selecionado el dia "+dia
-    d.getElementById("modalDia").classList.remove("hidden")
-    d.getElementById("modalDia").classList.add("flex")
+function mostrarModalDia(dia) {
+  const modal = d.getElementById("modalDia");
+
+  // Actualiza el título del modal
+  d.getElementById("tituloDia").textContent = "Día " + dia;
+
+  // Muestra el modal
+  modal.classList.remove("hidden");
+  modal.classList.add("flex");
 }
 
-function cerrarModalDia(dia){
-    d.getElementById("modalDia").classList.add("hidden")
-    d.getElementById("modalDia").classList.remove("flex")
+function mostrarModalSolicitud() {
+  let modal = d.getElementById("modalSolicitud");
+
+  modal.classList.remove("hidden");
+  modal.classList.add("flex");
+}
+
+function mostrarCambioFecha() {
+  let zona = d.getElementById("cambioFecha");
+
+  zona.classList.remove("hidden");
+}
+
+function cerrarModalDia() {
+  const modal = d.getElementById("modalDia");
+  modal.classList.add("hidden");
+  modal.classList.remove("flex");
+}
+
+function cerrarModalSolicitud() {
+  const modal = d.getElementById("modalSolicitud");
+  modal.classList.add("hidden");
+  modal.classList.remove("flex");
 }
 
 d.addEventListener("DOMContentLoaded", (e) => {
-    crearCalendario();
-})
+  crearCalendario();
+
+  let modalSolicitud = d.getElementById("modalSolicitud");
+  modalSolicitud.addEventListener("click", (e) => {
+    if (e.target === modalSolicitud) {
+      modalSolicitud.classList.add("hidden");
+    }
+  });
+});
