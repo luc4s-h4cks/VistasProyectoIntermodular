d532771577eb40f2aed800e08f8ce0c7a892435f
lucas curiel <lcurgom2406@iesmarquesdecomares.org>
2026-02-24 21:18:12 +0100

    api con autenticacion ya funcionando

--- reservapp/routes/api.php
+++ reservapp/routes/api.php
@@ -3,9 +3,15 @@
 use App\Http\Controllers\TallerControllerApi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('talleres', TallerControllerApi::class);
+// Rutas públicas
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::apiResource('talleres', TallerControllerApi::class)->middleware('auth:sanctum');
